0316e88540f82e6a2e9ed4180ed5049192918df6
Raisa Meneses <raisameneses1@gmail.com>
2016-06-29 22:22:29 -0700

    Program asks the user for array size, and integers. Prints distinct

--- printDistinctNumbers/printDistinctcpp.cpp
+++ printDistinctNumbers/printDistinctcpp.cpp
@@ -1,6 +1,43 @@
+//Raisa Meneses
+//this program reads an int array and displays unique numbers in the array
 #include<iostream>
 using namespace std;
+ 
 int main()
 {
-	cout << "Raisa";
+	int size; 
+	cout << "enter array size: "; 
+	cin >> size; 
+	int* numbers = new int[size];
+
+	int totalSize = 0; 
+	for (int i = 0; i < size; i++)
+	{
+		cout << "enter an integer" << endl;
+		int newNumber; 
+		cin >> newNumber;
+
+		bool seenValue = false; 
+
+		for (int j = 0; j < totalSize; j++)
+		{
+			if (*(numbers + j) == newNumber)
+			{
+				seenValue = true;
+				break;
+			}
+		}
+
+		if (!seenValue)
+		{
+			*(numbers + totalSize) = newNumber;
+			totalSize++;
+		}
+	}
+	for (int i = 0; i < totalSize; i++)
+		cout << *(numbers + i) << " ";
+
+	return 0;
+
 }
+
